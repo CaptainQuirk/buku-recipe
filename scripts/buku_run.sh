be47e3c614651d476b20d002d525708262a4ed4a
lmessier <leonard.messier@gmail.com>
2018-12-07 11:46:25 +0100

    buku_run script does not need to be interactive

--- scripts/buku_run.sh
+++ scripts/buku_run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 CACHE_DIR=$HOME/.cache/configuration
-HANDLE=$1
-sleep 1
 if [ ! -d "$CACHE_DIR/buku/buku_run" ]; then
     git clone https://github.com/carnager/buku_run.git "$CACHE_DIR/buku/buku_run"
     (
@@ -12,8 +10,6 @@ if [ ! -d "$CACHE_DIR/buku/buku_run" ]; then
     )
 else
   echo "$CACHE_DIR/buku/buku_run already exists. Nothing to do !"
-  sleep 2
 fi
 
-echo 0 > "$HANDLE"
 exit 0
