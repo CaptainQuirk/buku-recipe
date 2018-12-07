93b662b689af56cb0dadb60a6cbc6283134bb873
lmessier <leonard.messier@gmail.com>
2018-12-07 11:43:22 +0100

    finishing up script externalization

--- scripts/buku_run.sh
+++ scripts/buku_run.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 CACHE_DIR=$HOME/.cache/configuration
+HANDLE=$1
+sleep 1
 if [ ! -d "$CACHE_DIR/buku/buku_run" ]; then
     git clone https://github.com/carnager/buku_run.git "$CACHE_DIR/buku/buku_run"
-    cd "$CACHE_DIR/buku/buku_run" || exit 1
-    sudo make install
-    cd - || exit 1
+    (
+      cd "$CACHE_DIR/buku/buku_run"
+      sudo make install
+      cd -
+    )
+else
+  echo "$CACHE_DIR/buku/buku_run already exists. Nothing to do !"
+  sleep 2
 fi
 
+echo 0 > "$HANDLE"
 exit 0
--- scripts/bukubrow.sh
+++ scripts/bukubrow.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 CACHE_DIR=$HOME/.cache/configuration
-DROPBOX_DIR=$HOME/Dropbox
-BUKU_BOOKMARK_DIR=$HOME/.local/share/buku
 WWW_CONFIG_PATH=$HOME/.config/www
 
 mkdir -p "$CACHE_DIR/buku"
+mkdir -p "$WWW_CONFIG_PATH"
 
 BUKUBROW_CACHE_DIR=$CACHE_DIR/buku/bukubrow/
 mkdir -p "$BUKUBROW_CACHE_DIR"
@@ -14,12 +13,16 @@ if [ ! -f "$WWW_CONFIG_PATH/NativeMessagingHosts/com.samhh.bukubrow.json" ]; the
         curl -sSfL "https://github.com/SamHH/bukubrow/releases/download/2.3.0/binary-linux-x64.zip" -o "$BUKUBROW_CACHE_DIR/binary-linux-x64-2.3.0.zip"
     fi
 
-    cd "$BUKUBROW_CACHE_DIR" || exit 1
-    unzip binary-linux-x64-2.3.0.zip
+    cd "$BUKUBROW_CACHE_DIR"
+    unzip -o binary-linux-x64-2.3.0.zip
     chmod +x install.sh
     ./install.sh
     cd - || exit 1
 
-    mv "$HOME/.config/google-chrome/NativeMessagingHosts/com.samhh.bukubrow.json" "$WWW_CONFIG_PATH/NativeMessagingHosts"
+    if [ -f "$HOME/.config/google-chrome/NativeMessagingHosts/com.samhh.bukubrow.json" ]; then
+      mv "$HOME/.config/google-chrome/NativeMessagingHosts/com.samhh.bukubrow.json" "$WWW_CONFIG_PATH/NativeMessagingHosts/"
+    fi
 fi
+echo 0 > "$HOME/.cache/shef/script/terminal-handle"
 
+exit 0
